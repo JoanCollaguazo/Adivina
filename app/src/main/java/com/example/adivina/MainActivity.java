f3ecb154cec52693501f5718ab1ceeee30d58d86
OxOrboy <joangamers12@gmail.com>
2021-07-13 15:25:02 -0500

    PROYECTO ANDROID - COMMIT 1

---
+++ app/src/main/java/com/example/adivina/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.adivina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtAdivina;
+    private Button btnCheck;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtAdivina = (TextView) findViewById(R.id.editTextAdivina);
+        btnCheck = (Button) findViewById(R.id.buttonCheck);
+
+    }
+
+    public int aleatorio(int a) {
+        Random nAleatorio = new Random();
+        int n = nAleatorio.nextInt(a);
+        System.out.println("EL NUMERO ALEATORIO ES: " + n);
+        return a;
+    }
+
+}
