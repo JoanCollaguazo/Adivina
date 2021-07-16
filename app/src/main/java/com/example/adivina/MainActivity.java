96556cc4f106b6165ed095ad8007e0b55cd71240
OxOrboy <joangamers12@gmail.com>
2021-07-16 09:32:00 -0500

    INTENTS

--- app/src/main/java/com/example/adivina/MainActivity.java
+++ app/src/main/java/com/example/adivina/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.adivina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView txtAdivina;
     private Button btnCheck;
@@ -20,14 +23,33 @@ public class MainActivity extends AppCompatActivity {
 
         txtAdivina = (TextView) findViewById(R.id.editTextAdivina);
         btnCheck = (Button) findViewById(R.id.buttonCheck);
+        btnCheck.setOnClickListener(this);
 
     }
-//METODO RANDOM:
-    public int aleatorio(int a) {
-        Random nAleatorio = new Random();
-        int n = nAleatorio.nextInt(a);
-        System.out.println("EL NUMERO ALEATORIO ES: " + n);
-        return a;
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnCheck) {
+            System.out.println("HOLA ESTOY DANDO CLICK");
+            if (txtAdivina.getText().toString().isEmpty()) {
+                txtAdivina.setError("escribe un numero");
+            } else {
+                iniciarIntent();
+            }
+        }
     }
+    public void iniciarIntent(){
+        try {
+            int num=Integer.valueOf(txtAdivina.getText().toString());
+            Intent intent = new Intent(MainActivity.this,ResultadoActivity.class);
+            intent.putExtra("number",num);
+            startActivity(intent);
+
+        }catch (NumberFormatException num){
+            txtAdivina.setError(getResources().getString(R.string.error_numero));
+        }
+
+    }
+
 
 }
--- app/src/main/java/com/example/adivina/ResultadoActivity.java
+++ app/src/main/java/com/example/adivina/ResultadoActivity.java
@@ -2,13 +2,48 @@ package com.example.adivina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ResultadoActivity extends AppCompatActivity{
+import java.util.Random;
+
+public class ResultadoActivity extends AppCompatActivity {
+
+    private TextView txtRespuesta;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+        int numAd = 0;
+        Bundle extra = getIntent().getExtras();
+        numAd = extra.getInt("number");
+        verificar(numAd);
+        txtRespuesta = (TextView) findViewById(R.id.txtresultado);
+
     }
+
+    public void verificar(int num) {
+
+        int nAl = aleatorio(3);
+
+        if (num == nAl) {
+            txtRespuesta.setText("GANASTE!");
+            txtRespuesta.setTextColor(Color.GREEN);
+        } else {
+            txtRespuesta.setText("PERDISTE!");
+            txtRespuesta.setTextColor(Color.RED);
+        }
+    }
+
+    //METODO RANDOM:
+    public int aleatorio(int a) {
+        Random nAleatorio = new Random();
+        int n = nAleatorio.nextInt(a);
+        return n;
+    }
+
 }
